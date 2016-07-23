c9d3d243b08d4cb7670578b7a77473ce3efde368
James Blachly <james.blachly@gmail.com>
2016-07-22 22:27:27 -0400

    working except not multiple with comma

--- vcf_ann.py
+++ vcf_ann.py
@@ -1,13 +1,45 @@
-def decode(ann_string):
-"""
-Decodes VCF ANN field standard 1.0
-http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+import unittest
 
-input: annotation string (either with or without ANN= prefix)
-output: dictionary representing annotations
-    absent elements in the ANN field will not have
-    a corresponding key in the dict
-"""
+class TestAnnDecode(unittest.TestCase):
+    # Test annotations
+    ANN1 = "C|stop_gained|HIGH|BTK|ENSG...|transcript|featid|Coding|7|c.123T>C|p.L456R|123|234|456|-100|W1"
+    ANN2 = "T|histone_binding_site|LOW|BTK|ENSG...|transcript|featid|Coding|7/10|c.123T>C|p.L456R|123/1000|234/900|456/500|-100|WARNING_REF_DOES_NOT_MATCH_GENOME"
+    ANN3 = "C|intron_variant&nc_transcript_variant|MEDIUM|||transcript|featid|Coding|7/10|c.123T>C|p.L456R|123|234|456|-100|W1"
+    ANN_multiple = ANN1 + ", " + ANN3
+    ANN_weq = "ANN=" + ANN1
+
+    # Results
+    RES1 = {'distance_to_feature': '-100', 'gene_name': 'BTK', 'feature_type': 'transcript', 'protein_position': '456', 'errors_warnings_info': 'W1', 'feature_id': 'featid', 'rank_total': '7', 'gene_id': 'ENSG...', 'hgvs.p': 'p.L456R', 'transcript_biotype': 'Coding', 'cds_position': '234', 'cdna_position': '123', 'annotation': 'stop_gained', 'putative_impact': 'HIGH', 'allele': 'C', 'hgvs.c': 'c.123T>C'}
+    RES2 = {'putative_impact': 'LOW', 'errors_warnings_info': 'WARNING_REF_DOES_NOT_MATCH_GENOME', 'cdna_position': '123/1000', 'hgvs.p': 'p.L456R', 'distance_to_feature': '-100', 'rank_total': '7/10', 'protein_position': '456/500', 'feature_id': 'featid', 'cds_position': '234/900', 'annotation': 'histone_binding_site', 'allele': 'T', 'transcript_biotype': 'Coding', 'gene_id': 'ENSG...', 'feature_type': 'transcript', 'hgvs.c': 'c.123T>C', 'gene_name': 'BTK'}
+    RES3 = {'feature_id': 'featid', 'protein_position': '456', 'gene_id': '', 'rank_total': '7/10', 'transcript_biotype': 'Coding', 'hgvs.c': 'c.123T>C', 'distance_to_feature': '-100', 'feature_type': 'transcript', 'hgvs.p': 'p.L456R', 'errors_warnings_info': 'W1', 'cds_position': '234', 'annotation': 'intron_variant&nc_transcript_variant', 'putative_impact': 'MEDIUM', 'cdna_position': '123', 'gene_name': '', 'allele': 'C'}
+
+    RES_multiple = {}
+
+    def test_decode_basic(self):
+        self.assertEqual(decode(self.ANN1), self.RES1)
+    
+    def test_decode_range(self):
+        self.assertEqual(decode(self.ANN2), self.RES2)
+
+    def test_decode_multiple_conseq(self):
+        self.assertEqual(decode(self.ANN3), self.RES3)
+
+    def test_decode_weq(self):
+        self.assertEqual(decode(self.ANN_weq), self.RES1)
+
+        
+
+
+def decode(ann_string):
+    """
+    Decodes VCF ANN field standard 1.0
+    http://snpeff.sourceforge.net/VCFannotationformat_v1.0.pdf
+
+    input: annotation string (either with or without ANN= prefix)
+    output: dictionary representing annotations
+        absent elements in the ANN field will not have
+        a corresponding key in the dict
+    """
 
     if not ann_string: return dict()
 
@@ -15,7 +47,7 @@ output: dictionary representing annotations
     if ann_string[0:4] == "ANN=": ann_string = ann_string[4:]
 
     # specs page 2:
-    # Multiple “effects / consequences” are separated by comma.
+    # Multiple effects / consequences are separated by comma.
     ann_list = ann_string.split(',')
 
     # TODO: set up loop. for now examine first only
@@ -28,7 +60,7 @@ output: dictionary representing annotations
 
     result = dict()
 
-    # TODO: read field order from header. FOr now, define statically
+    # TODO: read field order from header. For now, define statically
     field_list = [  "allele",
                     "annotation",
                     "putative_impact",
@@ -51,9 +83,9 @@ output: dictionary representing annotations
         fields[j] = i
 
 
-    for i,field in enumerate(eff):
-	result[ field ] = eff[i]
+    for i,value in enumerate(eff):
+        result[ field_list[i] ] = value
 
     # Done, return object
-    print result
+    #print(result)
     return result
